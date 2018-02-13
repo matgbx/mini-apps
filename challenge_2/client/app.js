4780be28a9dc712a76bd4bb149b62363cb66e1c1
Mat Bagnall <matbagnall@gmail.com>
2018-02-13 12:36:47 -0800

    started server and attempting to set up ajax post request upon submit

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,21 @@
 
+// this file will hold all of the functionality for the client side
 
+//use jquery 
+
+//submit button on click will submit a post request using ajax
+
+$('.submitBtn').on('click', () => {
+  $.ajax({
+    'url': '/',
+    'method': 'POST',
+    'content-type': 'application/json',
+    'data':,
+    success: (data) => {
+      console.log('successfully sent', data);
+    },
+    error: (error) => {
+      console.log('this shit didn\'t work!!', error)
+    }
+  })
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+// Express and server connection etc
 
+//assuming that we are using MySql??
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('connected to server'));
+
+// app.use('/class', express.static(__dirname, './client'));
+
+app.listen(3000, () => console.log('listening on port 3000'))
 
